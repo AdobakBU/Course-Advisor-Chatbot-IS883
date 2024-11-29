577c14fd63d0acb41d7ec0fa3752aed284a85802
AdobakBU <adobak@bu.edu>
2024-11-29 14:54:07 -0800

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,19 +111,21 @@ if "memory" not in st.session_state: ### IMPORTANT.
         return "Today is " + str(date.today())
 
     tools = [datetoday]
-    
+
     # Now we add the memory object to the agent executor
     # prompt = hub.pull("hwchase17/react-chat")
     # agent = create_react_agent(chat, tools, prompt)
     from langchain_core.prompts import ChatPromptTemplate
+
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "You are a helpful assistant."),
+            ("system", "You are a helpful assistant"),
             ("placeholder", "{chat_history}"),
             ("human", "{input}"),
             ("placeholder", "{agent_scratchpad}"),
         ]
     )
+
     agent = create_tool_calling_agent(chat, tools, prompt)
     st.session_state.agent_executor = AgentExecutor(agent=agent, tools=tools,  memory=st.session_state.memory, verbose= True)  # ### IMPORTANT to use st.session_state.memory and st.session_state.agent_executor.
 
