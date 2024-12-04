8c6765cb988e80858ccec2da9a2a56db33abe4b7
AdobakBU <adobak@bu.edu>
2024-12-04 11:44:47 -0500

    fsdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,14 +29,7 @@ LOGO_URL_LARGE = "https://raw.githubusercontent.com/AdobakBU/Course-Advisor-Chat
 st.logo(LOGO_URL_LARGE, size='large')
 
 # Show title and description.
-st.title(("""\
-   ____ _                ___                 _              ____ ____ _____ 
-  / ___| | __ _ ___ ___ / _ \ _   _  ___ ___| |_           / ___|  _ |_   _|
- | |   | |/ _` / __/ __| | | | | | |/ _ / __| __|  _____  | |  _| |_) || |  
- | |___| | (_| \__ \__ | |_| | |_| |  __\__ | |_  |_____| | |_| |  __/ | |  
-  \____|_|\__,_|___|___/\__\_\\__,_|\___|___/\__|          \____|_|    |_|  
-
-                    """))
+st.title("ClassQuest-GPT")
 st.subheader("To help with class discovery and comparison")
 
 # Define the number of top matching chunks to retrieve
